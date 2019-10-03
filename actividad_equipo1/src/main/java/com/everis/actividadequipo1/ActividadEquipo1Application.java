d8eb4019c32c3c049e80da432ffc78276b7da3a4
YASMIN <rivera_15yas@outlook.com>
2019-10-03 09:48:19 -0500

    Creacion del proyecto en Spring

---
+++ actividad_equipo1/src/main/java/com/everis/actividadequipo1/ActividadEquipo1Application.java
@@ -0,0 +1,13 @@
+package com.everis.actividadequipo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadEquipo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadEquipo1Application.class, args);
+	}
+
+}

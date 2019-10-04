1f1320abf27e8dd16016b2f448925b4c9dd62c74
YASMIN <rivera_15yas@outlook.com>
2019-10-04 11:30:05 -0500

    Documentacion

--- Inventario/src/main/java/com/everis/practicaequipo1/inventario/InventarioApplication.java
+++ Inventario/src/main/java/com/everis/practicaequipo1/inventario/InventarioApplication.java
@@ -2,12 +2,24 @@ package com.everis.practicaequipo1.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class InventarioApplication {
+@Import(SwaggerConfiguration.class)
+public class InventarioApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
 	}
+	
+	@Override
+	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	         registry.addResourceHandler("swagger-ui.html")
+	                  .addResourceLocations("classpath:/META-INF/resources/");
+	  }
+
 
 }
--- actividad_equipo1/src/main/java/com/everis/actividadequipo1/ActividadEquipo1Application.java
+++ actividad_equipo1/src/main/java/com/everis/actividadequipo1/ActividadEquipo1Application.java
@@ -5,15 +5,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class ActividadEquipo1Application {
+@Import(SwaggerConfiguration.class)
+public class ActividadEquipo1Application implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActividadEquipo1Application.class, args);
 	}
+	
+	 @Override
+	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	         registry.addResourceHandler("swagger-ui.html")
+	                  .addResourceLocations("classpath:/META-INF/resources/");
+	  }
+
 
 }
